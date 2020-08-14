1154d647f48ce4c204c3cebe7613cf2014fe8c73
Aniket Devle <aniketdevle@gmail.com>
2020-08-15 00:32:04 +0530

    Create Amazon_scrapping.py

---
+++ Amazon_scrapping.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+URL = "https://www.amazon.com/s?k=Mobile+Phones"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Linux; Android 8.1.0; Pixel 2 XL Build/OPM1.171019.013) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.111 Mobile Safari/537.36",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8", "DNT": "1",
+    "Connection": "close", "Upgrade-Insecure-Requests": "1"
+}
+
+page = requests.get(URL , headers = headers)
+soup = BeautifulSoup(page.text, 'html.parser')
+phones = soup.find_all('div' , attrs= {'class' : 's-result-item'})
+
+
+a = []
+b = []
+c = []
+
+for phone in phones:
+    name = phone.find_all('span', attrs={'class': 'a-size-base a-color-base a-text-normal'})
+    price = phone.find_all('span' , attrs = {'class' : 'a-offscreen'})
+    rating = phone.find_all('span' , attrs = {'class' : 'a-icon-alt'})
+    if(len(name) > 0):
+        name = name[0].text
+        if len(price)>0:
+            price = price[0].text
+        else:
+            price = None
+
+        if len(rating) > 0:
+            rating = rating[0].text
+        else:
+            rating = None
+
+        a.append(name)
+        b.append(price)
+        c.append(rating)
+
+ans = []
+for i in range(len(a)):
+    ans.append([a[i] , b[i] , c[i]])
+
+
+df = pd.DataFrame(ans , columns= ['Name' , 'Price' , 'Rating'])
+
+ans = df.to_json(orient = 'records')
+
+print(ans)
